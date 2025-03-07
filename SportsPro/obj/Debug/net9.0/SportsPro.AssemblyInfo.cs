fce7a0011f6ce41e7d5a69d475d65fa72a9ca093
BunnyForPres <isaactheknight8@gmail.com>
2025-03-07 00:13:49 -0500

    ??

--- SportsPro/obj/Debug/net9.0/SportsPro.AssemblyInfo.cs
+++ SportsPro/obj/Debug/net9.0/SportsPro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6b49f57438eb959a59b54a5a07ee94d43aaabc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
